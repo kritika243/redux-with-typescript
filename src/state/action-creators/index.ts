a03b7ff9a8579c810c29deb1f37cd0447e573319
kritika243 <24kritikasharma24@gmail.com>
2021-11-30 16:32:50 +0530

    added action creator

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import { ActionType } from '../action-types'
+import { Action } from '../actions/index'
+
+const searchRepositories = (term: string) => {
+  return async (dispatch: any) => {
+    dispatch({
+      type: ActionType.SEARCH_REPOSITORIES,
+    })
+    try {
+    } catch (error: any) {
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_ERROR,
+        payload: error.message,
+      })
+    }
+  }
+}
